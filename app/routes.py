a2fdf99c5844cb8465a659272430569159a59182
Whelan W <christian.w@thoughtworks.com>
2019-05-14 15:47:58 -0400

    Added for loop to template and route, dummy posts right now

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,20 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Bill'}
-    return render_template('index.html', title='Welcome to the Fun Zone', user=user)
+    test_user1 = {'username': 'Jane'}
+    test_user2 = {'username': 'Maria'}
+    posts = [
+        {
+            'author': test_user1,
+            'body': 'Having a great day today!'
+        },
+        {
+            'author': test_user2,
+            'body': 'I really hate Jane'
+        },
+        {
+            'author': test_user2,
+            'body': 'This is a different Maria!'
+        }
+    ]
+    return render_template('index.html', title='Welcome to the Fun Zone', user=user, posts=posts)
