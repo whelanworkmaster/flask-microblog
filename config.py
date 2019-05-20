3fc57f636a594b94c10406a6c4fd32a615380188
Whelan W <christian.w@thoughtworks.com>
2019-05-20 15:27:05 -0400

    Working on local db

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir + 'app.db')
-    SQL_ALCHEMY_TRACK_MODIFICATIONS=False
+    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
