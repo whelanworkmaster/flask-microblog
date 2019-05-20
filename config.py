af015f21984da1ab64e7f233e86facfa7ed28fda
Whelan W <christian.w@thoughtworks.com>
2019-05-20 11:34:38 -0400

    First pass for flask sql alchemy and flask migrate

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir + 'app.db')
+    SQL_ALCHEMY_TRACK_MODIFICATIONS=False
